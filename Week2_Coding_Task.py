5b148125527c48ee53a70f1d3aa7d2bb27c39c59
chennweii <85962362+chennweii@users.noreply.github.com>
2025-03-08 21:11:06 +0800

    first commit

---
+++ Week2_Coding_Task.py
@@ -0,0 +1,17 @@
+import math
+
+A = input("Length of side A: ")
+B = input("Length of side B: ")
+C = input("Length of side C: ")
+
+if not A:
+    B, C = int(B), int(C) #Convert B and C to integer to be able to use the power operator **
+    missLength = math.sqrt((C**2) - (B**2))
+elif not B:
+    A, C = int(A), int(C)
+    missLength = math.sqrt((C**2) - (A**2))
+elif not C:
+    A, B = int(A), int(B)
+    missLength = math.sqrt((A**2) + (B**2))
+
+print(f"{missLength:.4f}")
