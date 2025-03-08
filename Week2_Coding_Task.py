313900a2b46e92defe642b91331ca19108fa3465
chennweii <85962362+chennweii@users.noreply.github.com>
2025-03-08 21:21:35 +0800

    Can see given sides at output now

--- Week2_Coding_Task.py
+++ Week2_Coding_Task.py
@@ -5,13 +5,19 @@ B = input("Length of side B: ")
 C = input("Length of side C: ")
 
 if not A:
-    B, C = int(B), int(C) #Convert B and C to integer to be able to use the power operator **
+    B, C = int(B), int(C) # Convert B and C to integer to be able to use the power operator **
     missLength = math.sqrt((C**2) - (B**2))
+    print(f"Given Side B = {B}, Side C = {C}") # Able to print Given sides now that the print statement is wrapped inside
+    print(f"Calculated: Side A = {missLength:.4f}")
 elif not B:
     A, C = int(A), int(C)
     missLength = math.sqrt((C**2) - (A**2))
+    print(f"Given Side A = {A}, Side C = {C}")
+    print(f"Calculated: Side B = {missLength:.4f}")
 elif not C:
     A, B = int(A), int(B)
     missLength = math.sqrt((A**2) + (B**2))
-
-print(f"{missLength:.4f}")
+    print(f"Given Side A = {A}, Side B = {B}")
+    print(f"Calculated: Side C = {missLength:.4f}")
+else:
+    print("All sides are given! Nothing to calculate here.")
